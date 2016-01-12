6cad950cd7acc185b54c013bf4df4de3ed168039
Faisal Abbas <90.abbasfaisal@gmail.com>
2016-01-13 00:57:52 +0500

    improve wrong login alert display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def validate_user
-    puts "\n\n\nI am here\n\n\n"
     user = User.where(:username => params[:session][:email], :password => params[:session][:password]).first
-    puts "\n\n\n\n\nafasdfds"
     puts user
     if user
-      redirect_to :controller => 'home', :action => 'index' if user
+      redirect_to :controller => 'home', :action => 'index'
       return
     end
-    flash[:alert] = "Invalid user/password combination"
+    flash[:alert] = "Invalid username or password"
     redirect_to :action => 'new'
   end
 end
